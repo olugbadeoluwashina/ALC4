e2e94def5a5990184690f70b7cbffbd890a145fa
Olugbade Oluwashina <olugbadeoluwashina@gmail.com>
2019-07-16 17:35:20 +0100

    Latest and finished

--- app/src/main/java/com/apriltech/apps/olugbadeshinaalc40/AboutActivity.java
+++ app/src/main/java/com/apriltech/apps/olugbadeshinaalc40/AboutActivity.java
@@ -1,5 +1,8 @@
 package com.apriltech.apps.olugbadeshinaalc40;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,8 +10,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
+
+    String weburl = "https://www.andela.com/alc/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +22,36 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         WebView aboutWebView = (WebView) findViewById(R.id.wbv_about);
-        aboutWebView.loadUrl("https://www.andela.com/alc");
+        aboutWebView.setWebViewClient(new MyWebView());
+        aboutWebView.getSettings().setJavaScriptEnabled(true);
+        aboutWebView.getSettings().setLoadsImagesAutomatically(true);
+        aboutWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        aboutWebView.loadUrl(weburl);
+    }
+
+    class MyWebView extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap icon) {
+            // TODO Auto-generated method
+            super.onPageStarted(view, url, icon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            if (Uri.parse(url).getHost().equals(weburl))
+                return false;
+
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+            startActivity(webIntent);
+            return true;
+
+        }
+
+
     }
 
 }
--- app/src/main/java/com/apriltech/apps/olugbadeshinaalc40/ProfileActivity.java
+++ app/src/main/java/com/apriltech/apps/olugbadeshinaalc40/ProfileActivity.java
@@ -1,9 +1,11 @@
 package com.apriltech.apps.olugbadeshinaalc40;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -22,9 +24,23 @@ public class ProfileActivity extends AppCompatActivity {
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+        TextView name, email, phone_number, slack;
+
+        name = findViewById(R.id.tvw_profile_name);
+        email =  findViewById(R.id.tvw3);
+        phone_number = findViewById(R.id.tvw4);
+        slack = findViewById(R.id.tvw5);
+
+        String output = "Name: "+name.getText().toString()+"\nEmail: "+email.getText().toString()+"\nPhone number: "+
+                phone_number.getText().toString()+"\nSlack: "+slack.getText().toString();
+
 		int itemId = item.getItemId();
 
 		if (itemId == R.id.menu_share) {
+		    Intent intent = new Intent(Intent.ACTION_SEND);
+		    intent.putExtra(Intent.EXTRA_TEXT, output);
+		    intent.setType("text/plain");
+		    startActivity(intent);
 		}
 
 		return super.onOptionsItemSelected(item);
